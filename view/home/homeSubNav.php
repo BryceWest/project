72b268b7ec8a180107bc0947b2b988b1494f13ad
brycejwest <brycejwest@gmail.com>
2013-06-03 16:23:44 -0700

    added a privacy list picker

--- view/home/homeSubNav.php
+++ view/home/homeSubNav.php
@@ -1,21 +1,38 @@
 
       <!-- sub navigation -->
-      <div class="span3">
-        <div class="well sidebar-nav">
-          <ul class="nav nav-list">
-            <li class="nav-header">Sidebar Category 1</li>
-            <li class="active"><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li class="nav-header">Sidebar Category 2</li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li class="nav-header">Sidebar Category 3</li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-          </ul>
-        </div><!--/.well -->
+      <div class="offset1 span2">
+        <div class="navbar">
+     <div class="navbar-inner">
+        <div class="nav-collapse collapse">
+
+
+            <ul class="nav pull-right">
+              <li id="fat-menu" class="dropdown">
+                <a href="#" id="drop3" role="button" class="dropdown-toggle" data-toggle="dropdown">Pick privacy list
+                  <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu" role="menu" aria-labelledby="drop3">
+<?php
+        foreach ( $privacy as $key => $item ) { ?>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php print $item["privacy1"]; ?></a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php print $item["privacy2"]; ?></a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php print $item["privacy3"]; ?></a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php print $item["privacy4"]; ?></a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><?php print $item["privacy5"]; ?></a></li>
+
+<?php   }
+?>
+
+
+
+
+
+                </ul>
+              </li>
+            </ul>
+        </div>
+      </div>
+              <?php print $privacy["privacy1"]; ?>
+    </div><!--/.well -->
       </div><!--/span-->
       <!-- end sub navigation -->
